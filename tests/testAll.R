4d21658b775bdedc8f6123ed69833cd4524afe74
Neobernad <Carrier>
2019-09-19 08:56:10 +0200

    Updating data from Bioconductor devel branch

--- tests/testAll.R
+++ tests/testAll.R
@@ -3,9 +3,9 @@ library(evaluomeR)
 data("rnaMetrics")
 
 dataFrame <- stability(data=rnaMetrics, k=2, bs=20, getImages = FALSE)
-dataFrame <- stabilityRange(data=rnaMetrics, k.range=c(2,5), bs=20, getImages = FALSE)
+dataFrame <- stabilityRange(data=rnaMetrics, k.range=c(2,3), bs=20, getImages = FALSE)
 
-dataFrame <- quality(data=rnaMetrics, k=2, getImages = FALSE)
+dataFrame <- quality(data=rnaMetrics, k=3, getImages = FALSE)
 dataFrame <- qualityRange(data=rnaMetrics, k.range=c(2,3), getImages = FALSE)
 assay(getDataQualityRange(dataFrame, 2), 1)
 
