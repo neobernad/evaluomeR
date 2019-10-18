8adf3a6d589db0b59f999cdb6e2e51728fa79590
neobernad <joseantonio.bernabe1@um.es>
2019-10-18 12:52:09 +0200

    Implemented getOptimalKValue. Also, metricsAnalysis no longer exists, it is been separated in three methods: plotMetricsMinMax, plotMetricsBoxplot and plotMetricsCluster

--- R/metricsAnalysis.R
+++ R/metricsAnalysis.R
@@ -1,32 +1,21 @@
-#' @title Global statistic analysis
-#' @name metricsAnalysis
-#' @aliases metricsAnalysis
+#' @title Minimum and maximum metric values plot.
+#' @name plotMetricsMinMax
+#' @aliases plotMetricsMinMax
 #' @description
-#' Global statistic analysis.
+#' It plots the minimum, maximum and standard deviation
+#' values of the metrics in a \code{\link{SummarizedExperiment}} object.
 #'
 #' @inheritParams stability
 #'
+#' @return Nothing.
 #'
 #' @examples
 #' # Using example data from our package
 #' data("ontMetrics")
-#' cor = metricsAnalysis(ontMetrics)
+#' plotMetricsMinMax(ontMetrics)
 #'
-metricsAnalysis <- function(data) {
-
+plotMetricsMinMax <- function(data) {
   data <- as.data.frame(assay(data))
-  cat("Plotting Min/Max/Sd plot...\n")
-  plotMinMax(data)
-  cat("Plotting boxplots...\n")
-  suppressWarnings(plotBloxplot(data))
-  cat("Plotting clustering...\n")
-  plotCluster(data)
-
-
-}
-
-## Private functions ##
-plotMinMax <- function(data) {
   # Prepare data for plotting
   # Data matrix without descritive column
   matrix = data.matrix(data[,-1])
@@ -79,7 +68,25 @@ plotMinMax <- function(data) {
     print(p)
 }
 
-plotBloxplot <- function(data) {
+
+#' @title Metric values as a boxplot.
+#' @name plotMetricsBoxplot
+#' @aliases plotMetricsBoxplot
+#' @description
+#' It plots the value of the metrics in a \code{\link{SummarizedExperiment}}
+#' object as a boxplot.
+#'
+#' @inheritParams stability
+#'
+#' @return Nothing.
+#'
+#' @examples
+#' # Using example data from our package
+#' data("ontMetrics")
+#' plotMetricsBloxplot(ontMetrics)
+#'
+plotMetricsBloxplot <- function(data) {
+  data <- as.data.frame(assay(data))
   num_metrics_plot=10
   data.metrics = data[,-1] # Removing Description column
 
@@ -112,7 +119,24 @@ plotBloxplot <- function(data) {
   }
 }
 
-plotCluster <- function(data) {
+#' @title Metric values clustering.
+#' @name plotMetricsCluster
+#' @aliases plotMetricsCluster
+#' @description
+#' It clusters the value of the metrics in a \code{\link{SummarizedExperiment}}
+#' object as a boxplot.
+#'
+#' @inheritParams stability
+#'
+#' @return Nothing.
+#'
+#' @examples
+#' # Using example data from our package
+#' data("ontMetrics")
+#' plotMetricsCluster(ontMetrics)
+#'
+plotMetricsCluster <- function(data) {
+  data <- as.data.frame(assay(data))
   data.metrics = data[,-1] # Removing Description column
   d <- dist(t(data.metrics), method = "euclidean") # distance matrix
   fit <- hclust(d, method="ward.D2")
@@ -122,3 +146,209 @@ plotCluster <- function(data) {
   print(p)
 }
 
+#
+# It returns true if value is in range (0.5, 0.7]
+#
+isReasonable <- function(value) {
+  return(value > 0.5 && value <= 0.7)
+}
+
+getLargestSilWidth <- function(qualityDf, metric, k1, k2) {
+  k1KSil = qualityDf[metric, k1]
+  k2KSil = qualityDf[metric, k2]
+  k = NULL
+  if (k1KSil >= k2KSil) {
+    k = k1
+  } else {
+    k = k2
+  }
+  return(getFormattedK(k))
+}
+
+#
+# It transform a string 'k_X' into 'X'.
+# For instace, input is 'k_4', output is '4'
+#
+getFormattedK <- function(k) {
+  return(gsub("^.*_","", k))
+}
+
+#' @title Calculating the optimal value of k.
+#' getOptimalKValue
+#' @aliases getOptimalKValue
+#' @description
+#' This method finds the optimal value of K per each metric.
+#'
+#' @param stabData An output \code{\link{SummarizedExperiment}} from
+#' a \code{\link{stabilityRange}} execution.
+#'
+#' @param qualData An output \code{\link{SummarizedExperiment}} from
+#' a \code{\link{qualityRange}} execution.
+#'
+#' @return It returns a dataframe following the schema:
+#' \code{metric}, \code{optimal_k}.
+#'
+#' @examples
+#' # Using example data from our package
+#' data("rnaMetrics")
+#' stabilityData <- stabilityRange(data=rnaMetrics, k.range=c(2,4), bs=20, getImages = FALSE)
+#' qualityData <- qualityRange(data=rnaMetrics, k.range=c(2,4), getImages = FALSE)
+#' kOptTable = getOptimalKValue(stabilityData, qualityData)
+#'
+getOptimalKValue <- function(stabData, qualData) {
+  checkStabilityQualityData(stabData, qualData)
+
+  stabDf = standardizeStabilityData(stabData)
+  qualDf = standardizeQualityData(qualData)
+
+  metrics = rownames(stabDf)
+  STABLE_CLASS = 0.75
+
+  outputTable = as.data.frame(metrics)
+  rownames(outputTable) = metrics
+  outputTable = outputTable[, -1]
+  optimalKs = list()
+
+  for (metric in metrics) {
+    cat("Processing metric: ", metric, "\n")
+    stabMaxK = colnames(stabDf[metric, ])[apply(stabDf[metric, ],1,which.max)] # k1
+    stabMaxKFormatted = getFormattedK(stabMaxK)
+    stabMaxVal = stabDf[metric, stabMaxK]
+    qualMaxK = colnames(qualDf[metric, ])[apply(qualDf[metric, ],1,which.max)] # k2
+    qualMaxVal = qualDf[metric, qualMaxK]
+    qualMaxKFormatted = getFormattedK(qualMaxK)
+    # CASE 1
+    if (length(stabMaxK) >= 1 && length(qualMaxK) >= 1 && identical(stabMaxK, qualMaxK)) {
+      k = stabMaxKFormatted
+      cat("\tMaximum stability and quality values matches the same K value: '", k ,"'\n")
+      optimalKs = append(optimalKs, k)
+    # CASE 2
+    } else if ((stabMaxVal >= STABLE_CLASS && stabDf[metric, qualMaxK] >= STABLE_CLASS) ||
+               (stabMaxVal < STABLE_CLASS && stabDf[metric, qualMaxK] < STABLE_CLASS)) {
+      if (stabMaxVal >= STABLE_CLASS && stabDf[metric, qualMaxK] >= STABLE_CLASS) {
+        cat("\tBoth Ks have a stable classification: '",
+            stabMaxKFormatted, "', '", qualMaxKFormatted ,"'\n")
+      } else {
+        cat("\tBoth Ks does not have a stable classification: '",
+            stabMaxKFormatted, "', '", qualMaxKFormatted ,"'\n")
+      }
+      k = getLargestSilWidth(qualDf, metric, stabMaxK, qualMaxK)
+      cat("\tUsing '", k, "' since it provides higher silhouette width\n")
+      optimalKs = append(optimalKs, k)
+    # CASE 3
+    } else if (stabMaxVal >= STABLE_CLASS && stabDf[metric, qualMaxK] < STABLE_CLASS) {
+      cat("\tStability k '", stabMaxKFormatted, "' is stable but quality k '",
+          qualMaxKFormatted,"' is not\n")
+      kSil = qualDf[metric, stabMaxK]
+      if (isReasonable(kSil)) {
+        cat("\tUsing ", stabMaxKFormatted, " since its silhouette width is at least reasonable\n")
+        optimalKs = append(optimalKs, stabMaxKFormatted)
+      } else {
+        k = getLargestSilWidth(qualDf, metric, stabMaxK, qualMaxK)
+        cat("\tUsing '", k, "' since it provides higher silhouette width\n")
+        optimalKs = append(optimalKs, k)
+      }
+    # CASE 4
+    } else if (stabMaxVal < STABLE_CLASS && stabDf[metric, qualMaxK] >= STABLE_CLASS) {
+      cat("\t Quality k '", qualMaxKFormatted, "' is stable but stability k '",
+          stabMaxKFormatted,"' is not\n")
+      kSil = qualDf[metric, qualMaxK]
+      if (isReasonable(kSil)) {
+        cat("\tUsing ", qualMaxKFormatted, " since its silhouette width is at least reasonable\n")
+        optimalKs = append(optimalKs, stabMaxKFormatted)
+      } else {
+        k = getLargestSilWidth(qualDf, metric, stabMaxK, qualMaxK)
+        cat("\tUsing '", k, "' since it provides higher silhouette width\n")
+        optimalKs = append(optimalKs, k)
+      }
+    } else { # This should not happen but it might come in handy to check errors
+      optimalKs = append(optimalKs, -1)
+    }
+  }
+
+  outputTable["Optimal_k"] = unlist(optimalKs)
+
+  return(outputTable)
+}
+
+checkStabilityQualityData <- function(stabData, qualData) {
+  stabDf = assay(stabData)
+  lengthStabDf = length(colnames(stabDf[,-1]))
+  stabRangeStart = gsub("^.*_.*_.*_","", colnames(stabDf[,-1])[1]) # Mean_stability_k_2 -> 2
+  stabRangeEnd = gsub("^.*_.*_.*_","", colnames(stabDf[,-1])[lengthStabDf])
+  lengthQual = length(qualData)
+  namesQual = names(qualData)
+  qualRangeStart = getFormattedK(namesQual[1]) # k_2 -> 2
+  qualRangeEnd = getFormattedK(namesQual[lengthQual])
+  if (stabRangeStart != qualRangeStart || stabRangeEnd != qualRangeEnd) {
+    stop("Stability data and quality data have different k ranges")
+  }
+  stabMetricsList = as.character(stabDf[,"Metric"])
+  qualMetricsList = as.character(
+    assay(getDataQualityRange(qualData, qualRangeStart))[,"Metric"]
+  )
+  if (!identical(stabMetricsList, qualMetricsList)) {
+    stop("Stability data and quality data have different metrics")
+  }
+}
+
+#
+# It transforms the output of qualityRange method
+# into a dataframe like this:
+# (rownames)       k_2       k_3       k_4
+# DegFact          0.6171262 0.6278294 0.4882649
+# ...
+# So that the input of getOptimalKValue has always a
+# standardized dataframe to process.
+#
+
+standardizeQualityData <- function(qualData) {
+  lengthQuality = length(qualData)
+  qualRangeStart = getFormattedK(names(qualData)[1])
+  qualRangeEnd = getFormattedK(names(qualData)[lengthQuality])
+  Metric = NULL
+  kValues = list()
+  for (i in seq(qualRangeStart, qualRangeEnd, 1)) {
+    curQual = as.data.frame(assay(getDataQualityRange(qualData, i)))
+    if (i == qualRangeStart) {
+      Metric = as.character(levels(curQual$Metric))
+    }
+    kValues[[i]] = as.numeric(as.character(curQual$Avg_Silhouette_Width))
+  }
+  qualDf = as.data.frame(Metric)
+  for (i in seq(qualRangeStart, qualRangeEnd, 1)) {
+    values = kValues[[i]]
+    newColname = paste0("k_", i)
+    qualDf[[newColname]] = values
+  }
+  rownames(qualDf) = qualDf$Metric
+  qualDf = qualDf[, -1] # Remove "Metric" column, metrics are rownames now
+  qualDf <- qualDf[ order(row.names(qualDf)), ]
+  return(qualDf)
+}
+
+#
+# It transforms the output of stabilityRange method
+# into a dataframe like this:
+# (rownames)       k_2       k_3       k_4
+# RIN              0.6171262 0.6278294 0.4882649
+# ...
+# So that the input of getOptimalKValue has always a
+# standardized dataframe to process.
+#
+standardizeStabilityData <- function(stabData) {
+  stabDf = as.data.frame(assay(stabData))
+  lengthColnames = length(colnames(stabDf))
+  for (i in seq(1, lengthColnames, 1)) {
+    colname = colnames(stabDf)[i]
+    newColname = gsub("^.*_.*_.*_","k_", colname)
+    colnames(stabDf)[i] = newColname
+    if (i != 1) { # Skip Metric column
+      stabDf[newColname] = as.numeric(as.character(stabDf[[newColname]]))
+    }
+  }
+  rownames(stabDf) = stabDf$Metric
+  stabDf = stabDf[, -1] # Remove "Metric" column, metrics are rownames now
+  stabDf <- stabDf[ order(row.names(stabDf)), ]
+  return(stabDf)
+}
--- tests/testAnalysis.R
+++ tests/testAnalysis.R
@@ -1,4 +1,13 @@
 library(evaluomeR)
 
 data("rnaMetrics")
-metricsAnalysis(rnaMetrics)
+plotMetricsMinMax(rnaMetrics)
+plotMetricsBoxplot(rnaMetrics)
+plotMetricsCluster(rnaMetrics)
+
+set.seed(100)
+stabilityData <- stabilityRange(data=rnaMetrics, k.range=c(2,4), bs=20, getImages = FALSE)
+set.seed(100)
+qualityData <- qualityRange(data=rnaMetrics, k.range=c(2,4), getImages = FALSE)
+
+kOptTable <- getOptimalKValue(stabilityData, qualityData)
