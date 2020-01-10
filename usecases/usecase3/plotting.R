4a4d516332bbc928802fe4371e1a3d81348cfde7
neobernad <joseantonio.bernabe1@um.es>
2020-01-10 13:02:07 +0100

    Adding usecase3 metaphor

--- usecases/usecase3/plotting.R
+++ usecases/usecase3/plotting.R
@@ -46,65 +46,86 @@ stabMultimc17 <- stabilityRange(data=inputMultimc17, k.range=c(2,15), bs=100, ge
 meanStabMatcomp15 = as.data.frame(assay(stabMatcomp15, "stability_mean"))
 meanStabMatcomp15[2:15] <- lapply(meanStabMatcomp15[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMatcomp15$Metric = "Matcomp15"
+meanStabMatcomp15$category = "Matcomp"
+meanStabMatcomp15$year = "2015"
 colnames(meanStabMatcomp15) = str_remove_all(colnames(meanStabMatcomp15), "Mean_stability_k_")
 
 meanStabMatcomp16 = as.data.frame(assay(stabMatcomp16, "stability_mean"))
 meanStabMatcomp16[2:15] <- lapply(meanStabMatcomp16[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMatcomp16$Metric = "Matcomp16"
+meanStabMatcomp16$category = "Matcomp"
+meanStabMatcomp16$year = "2016"
 colnames(meanStabMatcomp16) = str_remove_all(colnames(meanStabMatcomp16), "Mean_stability_k_")
 
 meanStabMatcomp17 = as.data.frame(assay(stabMatcomp17, "stability_mean"))
 meanStabMatcomp17[2:15] <- lapply(meanStabMatcomp17[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMatcomp17$Metric = "Matcomp17"
+meanStabMatcomp17$category = "Matcomp"
+meanStabMatcomp17$year = "2017"
 colnames(meanStabMatcomp17) = str_remove_all(colnames(meanStabMatcomp17), "Mean_stability_k_")
 
 meanStabMulti15 = as.data.frame(assay(stabMulti15, "stability_mean"))
 meanStabMulti15[2:15] <- lapply(meanStabMulti15[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMulti15$Metric = "Multi15"
+meanStabMulti15$category = "Multi"
+meanStabMulti15$year = "2015"
 colnames(meanStabMulti15) = str_remove_all(colnames(meanStabMulti15), "Mean_stability_k_")
 
 meanStabMulti16 = as.data.frame(assay(stabMulti16, "stability_mean"))
 meanStabMulti16[2:15] <- lapply(meanStabMulti16[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMulti16$Metric = "Multi16"
+meanStabMulti16$category = "Multi"
+meanStabMulti16$year = "2016"
 colnames(meanStabMulti16) = str_remove_all(colnames(meanStabMulti16), "Mean_stability_k_")
 
 meanStabMulti17 = as.data.frame(assay(stabMulti17, "stability_mean"))
 meanStabMulti17[2:15] <- lapply(meanStabMulti17[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMulti17$Metric = "Multi17"
+meanStabMulti17$category = "Multi"
+meanStabMulti17$year = "2017"
 colnames(meanStabMulti17) = str_remove_all(colnames(meanStabMulti17), "Mean_stability_k_")
 
 meanStabMultimc15 = as.data.frame(assay(stabMultimc15, "stability_mean"))
 meanStabMultimc15[2:15] <- lapply(meanStabMultimc15[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMultimc15$Metric = "Multimc15"
+meanStabMultimc15$category = "Multimc"
+meanStabMultimc15$year = "2015"
 colnames(meanStabMultimc15) = str_remove_all(colnames(meanStabMultimc15), "Mean_stability_k_")
 
 meanStabMultimc16 = as.data.frame(assay(stabMultimc16, "stability_mean"))
 meanStabMultimc16[2:15] <- lapply(meanStabMultimc16[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMultimc16$Metric = "Multimc16"
+meanStabMultimc16$category = "Multimc"
+meanStabMultimc16$year = "2016"
 colnames(meanStabMultimc16) = str_remove_all(colnames(meanStabMultimc16), "Mean_stability_k_")
 
 meanStabMultimc17 = as.data.frame(assay(stabMultimc17, "stability_mean"))
 meanStabMultimc17[2:15] <- lapply(meanStabMultimc17[2:15], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
 meanStabMultimc17$Metric = "Multimc17"
+meanStabMultimc17$category = "Multimc"
+meanStabMultimc17$year = "2017"
 colnames(meanStabMultimc17) = str_remove_all(colnames(meanStabMultimc17), "Mean_stability_k_")
 
 #### Stability plotting ----
 
-stabDfList = list(meanStabMatcomp15, meanStabMatcomp16)
+stabDfList = list(meanStabMatcomp15, meanStabMatcomp16, meanStabMatcomp17,
+                  meanStabMulti15, meanStabMulti16, meanStabMulti17,
+                  meanStabMultimc15, meanStabMultimc16, meanStabMultimc17)
 
 stabPlot = ggplot()
 
 for (stabDf in stabDfList) {
-  stabDf_melt = melt(stabDf, id.vars = "Metric")
+  stabDf_melt = melt(stabDf, id.vars = c("Metric", "category", "year"))
   colnames(stabDf_melt)[1] = "Journal"
   stabDf_melt$variable = as.integer(stabDf_melt$variable)
   stabPlot = stabPlot +
     geom_line(stabDf_melt,
-              mapping = aes(x=variable, y=value, group = 1, colour=Journal)) +
+              mapping = aes(x=variable, y=value, group = 1, linetype=category, colour=category)) +
     geom_point(stabDf_melt,
-               mapping = aes(x=variable, y=value, group = 1, colour=Journal))
+               mapping = aes(x=variable, y=value, group = 1, shape=year))
 }
 
 stabPlot +
   scale_x_continuous(name="k", breaks=1:14, labels=2:15) +
-  ylim(0,1)
+  scale_y_continuous(name="Stability", limits = c(0.5,1)) +
+  scale_colour_grey(start = 0, end = 0)
