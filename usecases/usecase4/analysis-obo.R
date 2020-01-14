85c3b6d01a36fe56abcaa250e37fe7f9199a3a07
Neobernad <joseantonio.bernabe1@um.es>
2020-01-14 16:15:07 +0100

    Updating usecase4

---
+++ usecases/usecase4/analysis-obo.R
@@ -0,0 +1,26 @@
+library(evaluomeR)
+
+wd = paste0(dirname(rstudioapi::getSourceEditorContext()$path),"/")
+inputPath=paste0(wd,"data/obo-119.csv")
+outputDir=paste0(wd,"results-obo")
+
+inputData = read.csv(inputPath, header = TRUE)
+
+# Stability [2,6]
+dataFrame <- stabilityRange(data=inputData, k.range=c(2,6), bs=100, getImages = TRUE, seed=13606)
+dataStability = assay(dataFrame, 1)
+csvPath = paste0(outputDir, "/stability.csv")
+write.csv(dataStability, csvPath, row.names = FALSE)
+
+# Quality [2,6]
+dataFrame <- qualityRange(data=inputData, k.range=c(2,6), getImages = TRUE, seed=13606)
+for (i in 2:6) {
+  data = assay(getDataQualityRange(dataFrame, i), 1)
+  csvPath = paste0(outputDir, "/quality_k=", i,".csv")
+  write.csv(data, csvPath, row.names = FALSE)
+}
+
+# Correlations
+dataFrame <- metricsCorrelations(inputData, margins=c(0,6,6,5), getImages=TRUE)
+csvPath = paste0(outputDir, "/agro.correlation",".csv")
+write.csv(assay(dataFrame, 1), csvPath, row.names = FALSE)
--- usecases/usecase4/plotting.R
+++ usecases/usecase4/plotting.R
@@ -10,6 +10,43 @@ getFormattedK <- function(k) {
   return(gsub("^.*_","", k))
 }
 
+standardizeStabilityData <- function(stabData, k.range=NULL) {
+  stabDf = as.data.frame(assay(stabData)) # Getting first assay, which is 'stabData$stability_mean'
+  lengthColnames = length(colnames(stabDf))
+  toRemove = list()
+  for (i in seq(1, lengthColnames, 1)) {
+    colname = colnames(stabDf)[i]
+    newColname = gsub("^.*_.*_.*_","k_", colname)
+    colnames(stabDf)[i] = newColname
+    if (i != 1) { # Skip Metric column
+      k = as.numeric(getFormattedK(newColname))
+      if (!is.null(k.range) && (k < k.range[1] || k > k.range[2])) {
+        toRemove = append(toRemove, newColname)
+        next
+      }
+      stabDf[newColname] = as.numeric(as.character(stabDf[[newColname]]))
+    }
+  }
+
+  for (columnName in toRemove) {
+    stabDf[, columnName] = list(NULL)
+    lengthColnames = lengthColnames-1
+  }
+
+  inputStartRange = as.numeric(getFormattedK(colnames(stabDf)[2]))
+  inputEndRange = as.numeric(getFormattedK(colnames(stabDf)[lengthColnames]))
+  if (!is.null(k.range) && (k.range[1] < inputStartRange || k.range[2] > inputEndRange)) {
+    # Input k.range is not a subset of the stabData k ranges
+    stop("Input k.range [", k.range[1], ", ", k.range[2], "] is not a subset of data range [",
+         inputStartRange, ", ", inputEndRange, "]")
+  }
+
+  rownames(stabDf) = stabDf$Metric
+  stabDf = stabDf[, -1] # Remove "Metric" column, metrics are rownames now
+  stabDf <- stabDf[ order(row.names(stabDf)), ]
+  return(stabDf)
+}
+
 standardizeQualityData <- function(qualData, k.range=NULL) {
   lengthQuality = length(qualData)
   qualRangeStart = getFormattedK(names(qualData)[1])
@@ -48,127 +85,57 @@ standardizeQualityData <- function(qualData, k.range=NULL) {
 
 
 #### Data loading ----
-matcomp15Path=paste0(wd,"data/data-matcomp15.csv")
-matcomp16Path=paste0(wd,"data/data-matcomp16.csv")
-matcomp17Path=paste0(wd,"data/data-matcomp17.csv")
-
-multi15Path=paste0(wd,"data/data-multi15.csv")
-multi16Path=paste0(wd,"data/data-multi16.csv")
-multi17Path=paste0(wd,"data/data-multi17.csv")
-
-multimc15Path=paste0(wd,"data/data-multimc15.csv")
-multimc16Path=paste0(wd,"data/data-multimc16.csv")
-multimc17Path=paste0(wd,"data/data-multimc17.csv")
+inputAgroPath=paste0(wd,"data/agro.csv")
+inputOboPath=paste0(wd,"data/obo-119.csv")
 outputDir=paste0(wd,"plots")
 
-inputMatcomp15 = read.csv(matcomp15Path, header = TRUE)
-inputMatcomp16 = read.csv(matcomp16Path, header = TRUE)
-inputMatcomp17 = read.csv(matcomp17Path, header = TRUE)
+agroData = read.csv(inputAgroPath, header = TRUE)
+oboData = read.csv(inputOboPath, header = TRUE)
+bothData = rbind(agroData, oboData)
 
-inputMulti15 = read.csv(multi15Path, header = TRUE)
-inputMulti16 = read.csv(multi16Path, header = TRUE)
-inputMulti17 = read.csv(multi17Path, header = TRUE)
 
-inputMultimc15 = read.csv(multimc15Path, header = TRUE)
-inputMultimc16 = read.csv(multimc16Path, header = TRUE)
-inputMultimc17 = read.csv(multimc17Path, header = TRUE)
-
-#### Stability [2,15] ----
-stabMatcomp15 <- stabilityRange(data=inputMatcomp15, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-stabMatcomp16 <- stabilityRange(data=inputMatcomp16, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-stabMatcomp17 <- stabilityRange(data=inputMatcomp17, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-
-stabMulti15 <- stabilityRange(data=inputMulti15, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-stabMulti16 <- stabilityRange(data=inputMulti16, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-stabMulti17 <- stabilityRange(data=inputMultimc17, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-
-stabMultimc15 <- stabilityRange(data=inputMultimc15, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-stabMultimc16 <- stabilityRange(data=inputMultimc16, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
-stabMultimc17 <- stabilityRange(data=inputMultimc17, k.range=c(3,15), bs=100, getImages = FALSE, seed=13606)
+#### Stability [2,6] ----
+stabAgro <- stabilityRange(data=agroData, k.range=c(2,6), bs=100, getImages = FALSE, seed=13606)
+stabObo <- stabilityRange(data=oboData, k.range=c(2,6), bs=100, getImages = FALSE, seed=13606)
+stabBoth <- stabilityRange(data=bothData, k.range=c(2,6), bs=100, getImages = FALSE, seed=13606)
 
 #### Stability SE to DF ----
+k.range=c(2,6)
+meanStabAgro = standardizeStabilityData(stabAgro, k.range)
+meanStabAgro$Metric = rownames(meanStabAgro)
+meanStabAgro$Repository = "AGRO"
+colnames(meanStabAgro) = str_remove_all(colnames(meanStabAgro), "k_")
 
-meanStabMatcomp15 = as.data.frame(assay(stabMatcomp15, "stability_mean"))
-meanStabMatcomp15[2:14] <- lapply(meanStabMatcomp15[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMatcomp15$Metric = "Matcomp15"
-meanStabMatcomp15$category = "MCB"
-meanStabMatcomp15$year = "2015"
-colnames(meanStabMatcomp15) = str_remove_all(colnames(meanStabMatcomp15), "Mean_stability_k_")
+meanStabObo = standardizeStabilityData(stabObo, k.range)
+meanStabObo$Metric = rownames(meanStabObo)
+meanStabObo$Repository = "OBO"
+colnames(meanStabObo) = str_remove_all(colnames(meanStabObo), "k_")
 
-meanStabMatcomp16 = as.data.frame(assay(stabMatcomp16, "stability_mean"))
-meanStabMatcomp16[2:14] <- lapply(meanStabMatcomp16[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMatcomp16$Metric = "Matcomp16"
-meanStabMatcomp16$category = "MCB"
-meanStabMatcomp16$year = "2016"
-colnames(meanStabMatcomp16) = str_remove_all(colnames(meanStabMatcomp16), "Mean_stability_k_")
-
-meanStabMatcomp17 = as.data.frame(assay(stabMatcomp17, "stability_mean"))
-meanStabMatcomp17[2:14] <- lapply(meanStabMatcomp17[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMatcomp17$Metric = "Matcomp17"
-meanStabMatcomp17$category = "MCB"
-meanStabMatcomp17$year = "2017"
-colnames(meanStabMatcomp17) = str_remove_all(colnames(meanStabMatcomp17), "Mean_stability_k_")
-
-meanStabMulti15 = as.data.frame(assay(stabMulti15, "stability_mean"))
-meanStabMulti15[2:14] <- lapply(meanStabMulti15[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMulti15$Metric = "Multi15"
-meanStabMulti15$category = "MS"
-meanStabMulti15$year = "2015"
-colnames(meanStabMulti15) = str_remove_all(colnames(meanStabMulti15), "Mean_stability_k_")
-
-meanStabMulti16 = as.data.frame(assay(stabMulti16, "stability_mean"))
-meanStabMulti16[2:14] <- lapply(meanStabMulti16[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMulti16$Metric = "Multi16"
-meanStabMulti16$category = "MS"
-meanStabMulti16$year = "2016"
-colnames(meanStabMulti16) = str_remove_all(colnames(meanStabMulti16), "Mean_stability_k_")
-
-meanStabMulti17 = as.data.frame(assay(stabMulti17, "stability_mean"))
-meanStabMulti17[2:14] <- lapply(meanStabMulti17[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMulti17$Metric = "Multi17"
-meanStabMulti17$category = "MS"
-meanStabMulti17$year = "2017"
-colnames(meanStabMulti17) = str_remove_all(colnames(meanStabMulti17), "Mean_stability_k_")
-
-meanStabMultimc15 = as.data.frame(assay(stabMultimc15, "stability_mean"))
-meanStabMultimc15[2:14] <- lapply(meanStabMultimc15[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMultimc15$Metric = "Multimc15"
-meanStabMultimc15$category = "MCB+MS"
-meanStabMultimc15$year = "2015"
-colnames(meanStabMultimc15) = str_remove_all(colnames(meanStabMultimc15), "Mean_stability_k_")
-
-meanStabMultimc16 = as.data.frame(assay(stabMultimc16, "stability_mean"))
-meanStabMultimc16[2:14] <- lapply(meanStabMultimc16[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMultimc16$Metric = "Multimc16"
-meanStabMultimc16$category = "MCB+MS"
-meanStabMultimc16$year = "2016"
-colnames(meanStabMultimc16) = str_remove_all(colnames(meanStabMultimc16), "Mean_stability_k_")
-
-meanStabMultimc17 = as.data.frame(assay(stabMultimc17, "stability_mean"))
-meanStabMultimc17[2:14] <- lapply(meanStabMultimc17[2:14], function(x) {if(is.factor(x)) as.numeric(as.character(x)) else x})
-meanStabMultimc17$Metric = "Multimc17"
-meanStabMultimc17$category = "MCB+MS"
-meanStabMultimc17$year = "2017"
-colnames(meanStabMultimc17) = str_remove_all(colnames(meanStabMultimc17), "Mean_stability_k_")
+meanStabBoth = standardizeStabilityData(stabBoth, k.range)
+meanStabBoth$Metric = rownames(meanStabBoth)
+meanStabBoth$Repository = "AGRO+OBO"
+colnames(meanStabBoth) = str_remove_all(colnames(meanStabBoth), "k_")
 
 #### Stability plotting ----
 
-stabDfList = list(meanStabMatcomp15, meanStabMatcomp16, meanStabMatcomp17,
-                  meanStabMulti15, meanStabMulti16, meanStabMulti17,
-                  meanStabMultimc15, meanStabMultimc16, meanStabMultimc17)
+stabDfList = list(meanStabAgro, meanStabObo, meanStabBoth)
 
 stabPlot = ggplot()
 min = Inf
 max = -Inf
 for (stabDf in stabDfList) {
-  stabDf_melt = melt(stabDf, id.vars = c("Metric", "category", "year"))
-  colnames(stabDf_melt)[1] = "Journal"
+  stabDf = stabDf[which(stabDf$Metric == "ANOnto" | stabDf$Metric == "CBOOnto"), ]
+  stabDf_melt = melt(stabDf, id.vars = c("Metric", "Repository"))
   stabDf_melt$variable = as.integer(stabDf_melt$variable)
-  stabPlot = stabPlot +
-    geom_line(stabDf_melt,
-              mapping = aes(x=variable, y=value, group = 1, linetype=category, colour=category)) +
-    geom_point(stabDf_melt,
-               mapping = aes(x=variable, y=value, group = 1, shape=year))
+  for (metric in c("ANOnto", "CBOOnto")) {
+    current_melt = stabDf_melt[which(stabDf_melt$Metric == metric), ]
+    stabPlot = stabPlot +
+      geom_line(current_melt,
+                mapping = aes(x=variable, y=value, group = 1, linetype=Metric, colour=Metric)) +
+      geom_point(stabDf_melt,
+                mapping = aes(x=variable, y=value, group = 1, shape=Repository))
+  }
+
   if (min > min(stabDf_melt$value)) {
     min = min(stabDf_melt$value)
   }
@@ -178,109 +145,9 @@ for (stabDf in stabDfList) {
 }
 
 stabPlot +
-  scale_x_continuous(name="k", breaks=1:13, labels=3:15) +
-  scale_y_continuous(name="Stability", limits = c(min,max), breaks = seq(0.5, 1, 0.05), labels=seq(0.5, 1, 0.05)) +
+  scale_x_continuous(name="k", breaks=1:5, labels=2:6) +
+  scale_y_continuous(name="Stability", limits = c(min,max), breaks = seq(0.5, 1, 0.025), labels=seq(0.5, 1, 0.025)) +
   scale_colour_grey(start = 0.7, end = 0) +
   theme_calc()
 
-#### Quality [3,15] ----
-qualMatcomp15 <- qualityRange(data=inputMatcomp15, k.range=c(3,15), getImages = FALSE, seed=13606)
-qualMatcomp16 <- qualityRange(data=inputMatcomp16, k.range=c(3,15), getImages = FALSE, seed=13606)
-qualMatcomp17 <- qualityRange(data=inputMatcomp17, k.range=c(3,15), getImages = FALSE, seed=13606)
-
-qualMulti15 <- qualityRange(data=inputMulti15, k.range=c(3,15), getImages = FALSE, seed=13606)
-qualMulti16 <- qualityRange(data=inputMulti16, k.range=c(3,15), getImages = FALSE, seed=13606)
-qualMulti17 <- qualityRange(data=inputMultimc17, k.range=c(3,15), getImages = FALSE, seed=13606)
-
-qualMultimc15 <- qualityRange(data=inputMultimc15, k.range=c(3,15), getImages = FALSE, seed=13606)
-qualMultimc16 <- qualityRange(data=inputMultimc16, k.range=c(3,15), getImages = FALSE, seed=13606)
-qualMultimc17 <- qualityRange(data=inputMultimc17, k.range=c(3,15), getImages = FALSE, seed=13606)
-
-#### Quality SE to DF ----
-k.range=c(3,15)
-silMatcomp15 = standardizeQualityData(qualMatcomp15, k.range)
-silMatcomp15$Metric = "Matcomp15"
-silMatcomp15$category = "MCB"
-silMatcomp15$year = "2015"
-colnames(silMatcomp15) = str_remove_all(colnames(silMatcomp15), "k_")
-
-silMatcomp16 = standardizeQualityData(qualMatcomp16, k.range)
-silMatcomp16$Metric = "Matcomp16"
-silMatcomp16$category = "MCB"
-silMatcomp16$year = "2016"
-colnames(silMatcomp16) = str_remove_all(colnames(silMatcomp16), "k_")
-
-silMatcomp17 = standardizeQualityData(qualMatcomp17, k.range)
-silMatcomp17$Metric = "Matcomp17"
-silMatcomp17$category = "MCB"
-silMatcomp17$year = "2017"
-colnames(silMatcomp17) = str_remove_all(colnames(silMatcomp17), "k_")
-
-silMulti15 = standardizeQualityData(qualMulti15, k.range)
-silMulti15$Metric = "Multi15"
-silMulti15$category = "MS"
-silMulti15$year = "2015"
-colnames(silMulti15) = str_remove_all(colnames(silMulti15), "k_")
-
-silMulti16 = standardizeQualityData(qualMulti16, k.range)
-silMulti16$Metric = "Multi16"
-silMulti16$category = "MS"
-silMulti16$year = "2016"
-colnames(silMulti16) = str_remove_all(colnames(silMulti16), "k_")
-
-silMulti17 = standardizeQualityData(qualMulti17, k.range)
-silMulti17$Metric = "Multi17"
-silMulti17$category = "MS"
-silMulti17$year = "2017"
-colnames(silMulti17) = str_remove_all(colnames(silMulti17), "k_")
-
-silMultimc15 = standardizeQualityData(qualMultimc15, k.range)
-silMultimc15$Metric = "Multimc15"
-silMultimc15$category = "MCB+MS"
-silMultimc15$year = "2015"
-colnames(silMultimc15) = str_remove_all(colnames(silMultimc15), "k_")
-
-silMultimc16 = standardizeQualityData(qualMultimc16, k.range)
-silMultimc16$Metric = "Multimc16"
-silMultimc16$category = "MCB+MS"
-silMultimc16$year = "2016"
-colnames(silMultimc16) = str_remove_all(colnames(silMultimc16), "k_")
-
-silMultimc17 = standardizeQualityData(qualMultimc16, k.range)
-silMultimc17$Metric = "Multimc17"
-silMultimc17$category = "MCB+MS"
-silMultimc17$year = "2017"
-colnames(silMultimc17) = str_remove_all(colnames(silMultimc17), "k_")
-
-#### Quality plotting ----
-
-silDfList = list(silMatcomp15, silMatcomp16, silMatcomp17,
-                  silMulti15, silMulti16, silMulti17,
-                  silMultimc15, silMultimc16, silMultimc17)
-
-silPlot = ggplot()
-min = Inf
-max = -Inf
-for (silDf in silDfList) {
-  silDf_melt = melt(silDf, id.vars = c("Metric", "category", "year"))
-  colnames(silDf_melt)[1] = "Journal"
-  silDf_melt$variable = as.integer(silDf_melt$variable)
-  silPlot = silPlot +
-    geom_line(silDf_melt,
-              mapping = aes(x=variable, y=value, group = 1, linetype=category, colour=category)) +
-    geom_point(silDf_melt,
-               mapping = aes(x=variable, y=value, group = 1, shape=year))
-  if (min > min(silDf_melt$value)) {
-    min = min(silDf_melt$value)
-  }
-  if (max < max(silDf_melt$value)) {
-    max = max(silDf_melt$value)
-  }
-}
-
-silPlot +
-  scale_x_continuous(name="k", breaks=1:13, labels=3:15) +
-  scale_y_continuous(name="Quality", limits = c(min,max), breaks = seq(0.5, 1, 0.05), labels=seq(0.5, 1, 0.05)) +
-  scale_colour_grey(start = 0.7, end = 0) +
-  theme_calc()
 
