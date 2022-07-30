e753f6367b6026edceddfb30d842cae51fc9a75f
Neobernad <joseantonio.bernabe1@um.es>
2022-07-30 13:17:59 +0200

    Adding notebooks

---
+++ tests/testRSKC.R
@@ -0,0 +1,43 @@
+library(evaluomeR)
+library(RSKC)
+library(sparcl)
+
+data("rnaMetrics")
+
+intputDf = as.data.frame(assay(ontMetrics))
+intputDf$Description = NULL
+
+intputDf
+
+inputMatrix = as.matrix(intputDf)
+# Note, has to scale matrix otherwise plot (km.perm will not be showing different #non-zero Wjs)
+inputMatrix = scale(inputMatrix, TRUE, TRUE)
+
+# Getting tunning paremeter, 1 =< L1 =< sqrt(num.vars)
+wbounds = seq(2,sqrt(ncol(inputMatrix)), len=30)
+km.perm <- KMeansSparseCluster.permute(x,K=3,wbounds=wbounds,nperms=5)
+print(km.perm)
+plot(km.perm)
+print(km.perm$bestw)
+
+
+
+
+stabilityData <- stabilityRange(data=ontMetrics, k.range=c(3,4), bs=20, getImages = FALSE, seed=100)
+qualityData <- qualityRange(data=ontMetrics, k.range=c(3,4), getImages = FALSE, seed=100)
+optK <- getOptimalKValue(stabilityData, qualityData, k.range=c(3,4))
+
+
+for (L1 in c(2,5,10,15)) {
+  r3 = RSKC(assayOnt, 3, 0.1, L1 = L1, nstart = 200,
+            silent=TRUE, scaling = FALSE, correlation = FALSE)
+  cat(paste0("L1 value: ", L1,"\n"))
+  cat(names(r3$weights)[1], ": ", r3$weights[1],"\n")
+  cat(names(r3$weights)[2], ": ", r3$weights[2],"\n")
+  cat(names(r3$weights)[3], ": ", r3$weights[3],"\n")
+  cat("---\n")
+}
+
+
+data("optd")
+truedigit <- rownames(optd)
