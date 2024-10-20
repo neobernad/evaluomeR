3d9af7b985dab68d7429513c8c30317e9ce4c2a3
Neobernad <joseantonio.bernabe1@um.es>
2024-10-20 23:39:04 +0200

    Bug fixing

--- tests/testDataPreprocessing.R
+++ tests/testDataPreprocessing.R
@@ -19,14 +19,3 @@ evaluomeR::plotPCA_fviz_screeplot(r_pca$pca)
 evaluomeR::plotPCA_fviz_biplot(r_pca$pca)
 
 head(r_pca$dataset_ncp)
-
-
-
-
-
-L1 = getRSKCL1Boundry(dataset, k=2, seed=2)
-
-L1 = getRSKCAlpha(dataset, k=2, L1=2, seed=2)
-
-evaluomeR::evaluomeRSupportedCBI(
-)
